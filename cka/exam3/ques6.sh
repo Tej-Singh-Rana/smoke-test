e9ce2d9fa5ac8bb655634654b5a5f8054f214fb9
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-16 23:21:10 +0530

    Update ques6.sh

--- cka/exam3/ques6.sh
+++ cka/exam3/ques6.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
+kubectl taint node node01 env_type=production:NoSchedule
+
+
+kubectl run dev-redis --image=redis:alpine
+
+
+cat <<EOF > /root/ques6.yaml
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: prod-redis
+spec:
+  containers:
+  - name: prod-redis
+    image: redis:alpine
+  tolerations:
+  - effect: NoSchedule
+    key: env_type
+    operator: Equal
+    value: production  
+
+EOF
+
+kubectl apply -f /root/ques6.yaml
+
+kubectl get po -owide
 
