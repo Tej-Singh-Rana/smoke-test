7021acc4c534abcb2d9dda2902a0ce64aa537d45
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-16 21:47:39 +0530

    Update ques2.sh

--- cka/exam2/ques2.sh
+++ cka/exam2/ques2.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+cat <<EOF > /root/ques2.yaml
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  creationTimestamp: null
+  labels:
+    run: redis-storage
+  name: redis-storage
+spec:
+  containers:
+  - image: redis:alpine
+    name: redis-storage
+    volumeMounts:
+    - mountPath: /data/redis
+      name: temp-volume
+  volumes:
+  - name: temp-volume
+    emptyDir: {}
 
+EOF
+
+kubectl apply -f /root/ques2.yaml
