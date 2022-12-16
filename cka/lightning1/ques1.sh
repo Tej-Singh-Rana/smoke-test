889bd9ef391f93a2859b7afda0af04f8239c6a8d
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-16 23:06:32 +0530

    Update ques1.sh

--- cka/lightning1/ques1.sh
+++ cka/lightning1/ques1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Unholding 
+apt-mark unhold kubelet kubeadm kubectl
+
 kubectl drain controlplane --ignore-daemonsets
 apt update
-apt-get install kubeadm=1.25.0-00
+apt-get install kubeadm=1.25.0-00 -y 
 kubeadm upgrade plan v1.25.0
 kubeadm upgrade apply v1.25.0
-apt-get install kubelet=1.25.0-00
+apt-get install kubelet=1.25.0-00 -y
 systemctl daemon-reload
 systemctl restart kubelet
 kubectl uncordon controlplane
@@ -27,10 +30,14 @@ kubectl drain node01 --ignore-daemonsets
 echo "#######################################################"
 ssh node01
 
+# Unholding 
+apt-mark unhold kubelet kubeadm kubectl
+
+
 apt update
-apt-get install kubeadm=1.25.0-00
+apt-get install kubeadm=1.25.0-00 -y
 kubeadm upgrade node
-apt-get install kubelet=1.25.0-00
+apt-get install kubelet=1.25.0-00 -y 
 systemctl daemon-reload
 systemctl restart kubelet
 
