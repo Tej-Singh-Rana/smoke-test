df974517ea84ed31577c218cc82549df68489e38
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-16 18:19:04 +0530

    Create ques4.sh

---
+++ ckad/lightning1/ques4.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+cat <<EOF > /root/ques4.yaml
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: nginx-deploy
+  name: nginx-deploy
+  namespace: default
+spec:
+  replicas: 4
+  selector:
+    matchLabels:
+      app: nginx-deploy
+  strategy:
+    rollingUpdate:
+      maxSurge: 1
+      maxUnavailable: 2
+    type: RollingUpdate
+  template:
+    metadata:
+      labels:
+        app: nginx-deploy
+    spec:
+      containers:
+      - image: nginx:1.16
+        imagePullPolicy: IfNotPresent
+        name: nginx
+
+EOF
+
+
+kubectl apply -f /root/ques4.yaml
+
+kubectl set image deployment nginx-deploy nginx=nginx:1.17
+
+
+kubectl rollout undo deployment nginx-deploy
