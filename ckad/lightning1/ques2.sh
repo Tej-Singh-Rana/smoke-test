ebacc8c4dda54c164d8eba6a9161099e3fc6b5e6
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-16 18:20:21 +0530

    Update ques2.sh

--- ckad/lightning1/ques2.sh
+++ ckad/lightning1/ques2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cat <<EOF > /root/ques2.yaml
+---
+apiVersion: networking.k8s.io/v1
 kind: NetworkPolicy
 metadata:
   name: test-network-policy
@@ -21,6 +23,8 @@ spec:
       port: 80
 
 EOF
+
+kubectl apply -f /root/ques2.yaml 
 
 sleep 2
 
