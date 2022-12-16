a8ed5c5c3de5c51590605a619f5f9ab8367bffb5
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-16 23:17:33 +0530

    Update ques3.sh

--- cka/exam3/ques3.sh
+++ cka/exam3/ques3.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
 
+cat <<EOF > /root/ques3.yaml
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: multi-pod
+spec:
+  containers:
+  - image: nginx
+    name: alpha
+    env:
+    - name: name
+      value: alpha
+  - image: busybox
+    name: beta
+    command: ["sleep", "4800"]
+    env:
+    - name: name
+      value: beta
+
+EOF  
+
+
+kubectl apply -f /root/ques3.yaml
+
