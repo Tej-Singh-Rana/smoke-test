d2edfe73a6f99b42bcca34f759254d48bb19b317
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-16 23:19:28 +0530

    Update ques5.sh

--- cka/exam3/ques5.sh
+++ cka/exam3/ques5.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
 
+cat <<EOF > /root/ques5.yaml
+---
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: ingress-to-nptest
+  namespace: default
+spec:
+  podSelector:
+    matchLabels:
+      run: np-test-1
+  policyTypes:
+  - Ingress
+  ingress:
+  - ports:
+    - protocol: TCP
+      port: 80
+
+EOF
+
+kubectl apply -f /root/ques5.yaml
+
+
 
