2e91e96f727bd0833747a2b102fa87ab0b12ec5c
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-20 21:09:38 +0530

    Update ques5.sh

--- cka/lightning1/ques5.sh
+++ cka/lightning1/ques5.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+cat <<EOF > /root/ques5-pvc.yaml
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: mysql-alpha-pvc
+  namespace: alpha
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+  storageClassName: slow
+
+EOF
+
+kubectl apply -f /root/ques5-pvc.yaml
 
