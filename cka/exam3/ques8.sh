8640a78a9ac4ade8bc8d576ca628d50c902059f1
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-24 21:38:24 +0530

    Update ques8.sh

--- cka/exam3/ques8.sh
+++ cka/exam3/ques8.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+cat /root/CKA/super.kubeconfig | grep server
+
+sed -i 's/9999/6443/g' /root/CKA/super.kubeconfig
+
+echo "#############################################"
+
+echo "Port has changed!!" 
+
+cat /root/CKA/super.kubeconfig | grep server
+
+kubectl cluster-info --kubeconfig=/root/CKA/super.kubeconfig
 
