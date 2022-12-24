31da5ae45da5c3630b8805392605d160d105b686
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-24 21:11:47 +0530

    Update ques8.sh

--- cka/exam2/ques8.sh
+++ cka/exam2/ques8.sh
@@ -5,13 +5,11 @@ kubectl run nginx-critical --image=nginx --dry-run=client -o yaml > static.yaml
 
 scp static.yaml node01:/etc/kubernetes/manifests/
 
-ssh node01
+ssh node01 'cat /var/lib/kubelet/config.yaml | grep staticPodPath'
 
-cat /var/lib/kubelet/config.yaml | grep staticPodPath
+ssh node 'ls -l /etc/kubernetes/manifests/'
 
-ls -l /etc/kubernetes/manifests/
+echo "Check the status of the static pod" 
 
-exit
-
-kubectl get po -owide
+kubectl get po -o wide 
 
