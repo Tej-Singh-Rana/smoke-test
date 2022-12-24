275647d3c8e37682f55b6592b208796cf9bbd17f
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-24 21:39:38 +0530

    Update ques9.sh

--- cka/exam3/ques9.sh
+++ cka/exam3/ques9.sh
@@ -3,10 +3,13 @@
 
 kubectl scale deploy nginx-deploy --replicas=3
 
+kubectl get deploy nginx-deploy
 
 sed -i 's/kube-contro1ler-manager/kube-controller-manager/g' /etc/kubernetes/manifests/kube-controller-manager.yaml
 
+sleep 2
 
 kubectl get deploy
+
 
 
