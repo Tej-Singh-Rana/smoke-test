f4e4141b669d45223db3604f03ea7eb5e4d219b0
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-24 21:20:11 +0530

    Update ques2.sh

--- cka/exam3/ques2.sh
+++ cka/exam3/ques2.sh
@@ -3,3 +3,7 @@
 
 kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' > /root/CKA/node_ips
 
+echo "Content of the node_ips file"
+
+cat /root/CKA/node_ips
+
