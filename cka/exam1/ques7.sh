9475feefa787c343a8a64d6b83292fc7d6c81b40
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-28 14:32:18 +0530

    Create ques7.sh

---
+++ cka/exam1/ques7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl run --restart=Never --image=busybox static-busybox --dry-run=client -oyaml --command -- sleep 1000 > /etc/kubernetes/manifests/static-busybox.yaml
+
+kubectl get po 
+
