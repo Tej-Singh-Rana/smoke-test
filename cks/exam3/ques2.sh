bfd4bce62f1a434ed2a997f884d27d6ef7ab444d
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-12-29 23:03:53 +0530

    Create ques2.sh

---
+++ cks/exam3/ques2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat <<EOF > /etc/kubernetes/prod-audit.yaml
+---
+apiVersion: audit.k8s.io/v1
+kind: Policy
+rules:
+- level: Metadata
+  namespaces: ["prod"]
+  verbs: ["delete"]
+  resources:
+  - group: ""
+    resources: ["secrets"]
+
+EOF
+
