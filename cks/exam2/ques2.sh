187e3b89533612e61fafc9e37a3342b0d634a15e
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2023-01-04 13:37:08 +0530

    Create ques2.sh

---
+++ cks/exam2/ques2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cat <<EOF > /root/ques2-netpol.yaml
+---
+kind: NetworkPolicy
+apiVersion: networking.k8s.io/v1
+metadata:
+  name: allow-app1-app2
+  namespace: apps-xyz
+spec:
+  podSelector:
+    matchLabels:
+      tier: backend
+      role: db
+  ingress:
+  - from:
+    - podSelector:
+        matchLabels:
+          name: app1
+          tier: frontend
+    - podSelector:
+        matchLabels:
+          name: app2
+          tier: frontend
+
+EOF
+
+kubectl apply -f /root/ques2-netpol.yaml
+
