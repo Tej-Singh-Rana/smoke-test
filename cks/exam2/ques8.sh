d692f0b47fdfd326e34c98f5ba3b5ff332f95259
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2023-01-04 15:32:13 +0530

    Create ques8.sh

---
+++ cks/exam2/ques8.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat <<EOF > /root/CKS/simple-pod.yaml
+---
+apiVersion: v1
+kind: Pod
+metadata:
+   labels:
+    name: simple-webapp
+   name: simple-webapp-1
+spec:
+  containers:
+  image: kodekloud/webapp-delayed-start
+   name: simple-webapp
+   ports:
+   -
+    containerPort: 8080
+    securityContext:
+      capabilities:
+        add:
+         - NET_ADMIN
+
+EOF
+
+kubesec scan /root/CKS/simple-pod.yaml | head -n10
+
+kubesec scan /root/CKS/simple-pod.yaml > /root/CKS/kubesec-report.txt 
+
