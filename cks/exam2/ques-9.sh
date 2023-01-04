56c64ee6e96bc0f9c4b9c64643a5f9075a652a17
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2023-01-04 14:37:19 +0530

    Create ques-9.sh

---
+++ cks/exam2/ques-9.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Image: nginx" 
+trivy image --severity=CRITICAL nginx | grep -i total
+
+echo "Image: nginx:1.19" 
+trivy image --severity=CRITICAL nginx:1.19 | grep -i total
+
+echo "Image: nginx:1.17" 
+trivy image --severity=CRITICAL nginx:1.17 | grep -i total
+
+echo "Image: nginx:1.20" 
+trivy image --severity=CRITICAL nginx:1.20 | grep -i total
+
+echo "Image: gcr.io/google-containers/nginx" 
+trivy image --severity=CRITICAL gcr.io/google-containers/nginx | grep -i total
+
+echo "Image: bitnami/nginx:latest" 
+trivy image --severity=CRITICAL bitnami/nginx:latest | grep -i total
+
+kubectl run secure-nginx-pod --image=gcr.io/google-containers/nginx -n seth
+
+sleep 2
+kubectl get po -n seth
+
+sleep 2
+kubectl get po -n seth
+
