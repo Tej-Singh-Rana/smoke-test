a8acd5822fe656ec83457ef92c48b518e2139598
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2023-04-28 22:16:23 +0530

    Update ques9.sh

--- cks/exam2/ques9.sh
+++ cks/exam2/ques9.sh
@@ -15,7 +15,7 @@ trivy image --severity=CRITICAL nginx:1.20 | grep -i total
 echo "Image: gcr.io/google-containers/nginx" 
 trivy image --severity=CRITICAL gcr.io/google-containers/nginx | grep -i total
 
-echo "Image: bitnami/nginx:latest" 
+echo "Image: bitnami/jenkins:latest" 
 trivy image --severity=CRITICAL bitnami/jenkins:latest | grep -i total
 
 kubectl run secure-nginx-pod --image=gcr.io/google-containers/nginx -n seth
