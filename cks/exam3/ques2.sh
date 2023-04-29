b6382d5e3721fae47a36ff57c5a421bcf45014b9
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2023-04-29 20:04:55 +0530

    Update ques2.sh

--- cks/exam3/ques2.sh
+++ cks/exam3/ques2.sh
@@ -14,3 +14,43 @@ rules:
 
 EOF
 
+echo "##############################"
+
+echo "
+ - --audit-policy-file=/etc/kubernetes/prod-audit.yaml
+ - --audit-log-path=/var/log/prod-secrets.log
+ - --audit-log-maxage=30
+ "
+ 
+ echo "##############################"
+
+echo "Add volumes in kube-apiserver.yaml file"
+
+ echo "
+   - name: audit
+    hostPath:
+      path: /etc/kubernetes/prod-audit.yaml
+      type: File
+
+  - name: audit-log
+    hostPath:
+      path: /var/log/prod-secrets.log
+      type: FileOrCreate
+"
+
+echo "##############################"
+
+echo "Add volumemounts in kube-apiserver.yaml file"
+
+echo "
+  - mountPath: /etc/kubernetes/prod-audit.yaml
+    name: audit
+    readOnly: true
+  - mountPath: /var/log/prod-secrets.log
+    name: audit-log
+    readOnly: false
+"
+
+
+
+
