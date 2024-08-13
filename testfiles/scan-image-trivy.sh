4135daa9e6437595e76269e6019173850ba864a9
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2024-08-13 18:18:29 +0530

    Create scan-image-trivy.sh

---
+++ testfiles/scan-image-trivy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List of Docker images to scan
+IMAGES=(
+    "docker.io/kodekloud/webapp-delayed-start"
+    "docker.io/library/httpd:2-alpine"
+    "docker.io/library/nginx:1.16"
+    "docker.io/library/httpd:2.4.33"
+)
+
+# Number of times to run the command
+RUNS=30
+
+# Loop through each image
+for ((i = 1; i <= RUNS; i++)); do
+    echo "Run #$i"
+    for IMAGE in "${IMAGES[@]}"; do
+        echo "Scanning image: $IMAGE"
+        trivy image "$IMAGE"
+    done
+done
