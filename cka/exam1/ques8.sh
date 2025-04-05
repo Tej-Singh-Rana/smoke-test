ac383836701c5aafcd22dffe1de6f35a9df8fe7b
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2025-04-05 15:36:40 +0530

    Update ques8.sh

--- cka/exam1/ques8.sh
+++ cka/exam1/ques8.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-kubectl run temp-bus --image=redis:alpine --namespace=finance --restart=Never
+cat <<EOF > /root/ques12-pv.yaml
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv-analytics
+spec:
+  capacity:
+    storage: 100Mi
+  volumeMode: Filesystem
+  accessModes:
+    - ReadWriteMany
+  hostPath:
+      path: /pv/data-analytics
+EOF
 
+kubectl apply -f /root/ques12-pv.yaml
 
-kubectl get po -n finance
+echo " "
+echo "#####################################################"
+echo " "
 
+kubectl get pv
