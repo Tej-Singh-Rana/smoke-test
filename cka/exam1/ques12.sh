faf8dc3fdfbe288ecdb71f3e3de0a563554d7484
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2025-04-05 15:49:34 +0530

    Update ques12.sh

--- cka/exam1/ques12.sh
+++ cka/exam1/ques12.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-cat <<EOF > /root/ques12-pv.yaml
----
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: pv-analytics
-spec:
-  capacity:
-    storage: 100Mi
-  volumeMode: Filesystem
-  accessModes:
-    - ReadWriteMany
-  hostPath:
-      path: /pv/data-analytics
+helm repo ls 
 
-EOF
+helm ls -A
 
-kubectl apply -f /root/ques12-pv.yaml
-
-echo " "
-echo "#####################################################"
-echo " "
-
-kubectl get pv
+helm repo update
 
