9bff744cf3b59f8933cae8c58ecd75cc8d764e67
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2025-04-06 01:13:40 +0530

    Update ques11.sh

--- cka/exam3/ques11.sh
+++ cka/exam3/ques11.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
+echo 'net.bridge.bridge-nf-call-iptables = 1' >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.ip_forward
+sysctl net.bridge.bridge-nf-call-iptables
 
